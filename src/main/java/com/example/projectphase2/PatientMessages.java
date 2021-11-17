7a30e4be4119da5319bb9d87b16f489368a4dc56
etjohns8 <94153845+etjohns8@users.noreply.github.com>
2021-11-17 07:59:01 -0700

    Messaging Implemented
    Messaging system implemented

--- src/main/java/com/example/projectphase2/PatientMessages.java
+++ src/main/java/com/example/projectphase2/PatientMessages.java
@@ -2,17 +2,96 @@ package com.example.projectphase2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static com.example.projectphase2.HelloController.currentUser;
 
 public class PatientMessages {
+
+    String currentUserEmail;
     @FXML
     private Button patientInformation;
     @FXML
     private Button pastVisits;
     @FXML
     private Button patientMessages;
+    @FXML
+    private Button newMessageButton;
+    @FXML
+    private ComboBox subjectSelection;
+    @FXML
+    private TextArea bodyDisplay;
+    @FXML
+    private TextField senderEmail;
+    @FXML
+    public void initialize() {
+        setUpSubjects();
+    }
+
+    private void setUpSubjects(){
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+        //String getEmails = "SELECT subject FROM messages WHERE reciever <> '" + currentUser+"'";
+        String getCurrentUserEmail = "SELECT email FROM user_account WHERE username = '" + currentUser + "'";
+        try{
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(getCurrentUserEmail);
+
+            while(queryResult.next()){
+                //selectReciever.getItems().addAll(queryResult.getString(1));
+                currentUserEmail = queryResult.getString(1);
+
+            }
+            String getSubjects = "SELECT subject FROM messages WHERE reciever = '" + currentUserEmail+"'";
+            queryResult = statement.executeQuery(getSubjects);
+
+            while(queryResult.next()){
+                subjectSelection.getItems().addAll(queryResult.getString(1));
+
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    public void updateMessageBox(ActionEvent event) throws IOException{
+
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+        //String getEmails = "SELECT subject FROM messages WHERE reciever <> '" + currentUser+"'";
+        String getSelectedMessage = "SELECT body FROM messages WHERE reciever = '" + currentUserEmail + "' AND subject = '" +subjectSelection.getValue() + "'";
+        String getSelectedMessageSender = "SELECT sender FROM messages WHERE reciever = '" + currentUserEmail + "' AND subject = '" +subjectSelection.getValue() + "'";
+
+
+        try{
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(getSelectedMessage);
+
+            while(queryResult.next()){
+                //selectReciever.getItems().addAll(queryResult.getString(1));
+                bodyDisplay.setText(queryResult.getString(1));
+
+            }
+            queryResult = statement.executeQuery(getSelectedMessageSender);
+            while(queryResult.next()){
+                //selectReciever.getItems().addAll(queryResult.getString(1));
+                senderEmail.setText(queryResult.getString(1));
+
+            }
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 
     public void goPatientInformation(ActionEvent event)throws IOException {
         goNewScene("patient-landing.fxml");
@@ -22,6 +101,9 @@ public class PatientMessages {
     }
     public void goPatientMessages(ActionEvent event)throws IOException{
         goNewScene("patient-messages.fxml");
+    }
+    public void goNewMessagePatient(ActionEvent event) throws IOException{
+        goNewScene("new-message-patient.fxml");
     }
 
 
